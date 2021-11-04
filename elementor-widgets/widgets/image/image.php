a2e19508f1ce55a8e34a31d620eb5f23d29a5c88
WebsiteDons <42153624+WebsiteDons@users.noreply.github.com>
2021-11-04 03:26:27 -0400

    Create image.php

---
+++ elementor-widgets/widgets/image/image.php
@@ -0,0 +1,43 @@
+<?php
+defined('ABSPATH') || exit('CMSEnergizer.com');
+
+/*
+only need to change the middle string of the class name
+eg: Cmse_image_Widget 
+Cmse_myNewName_Widget
+*/
+
+class Cmse_image_Widget extends \Elementor\Widget_Base
+{
+	public function get_name() {
+		$name = str_replace(['Cmse_','_Widget'],'',__CLASS__);
+		return strtolower($name);
+	}
+	
+	public function get_title() {
+		return 'CMSE '.$this->get_name();
+	}
+	
+	public function get_icon() {
+		return $this->_register_controls()->icon;
+	}
+	
+	public function get_categories() {
+		return [$this->_register_controls()->category];
+	}
+	
+	protected function _register_controls() {
+		$icon = Cmse_Elementor_Widgets::fields($this->get_name(),$this);
+		
+		return (object)$icon;
+	}
+	
+	// front end output
+	protected function render() {
+		$v = (object)$this->get_settings_for_display();
+		include __DIR__.'/display.php';
+	}
+	
+	protected function content_template() {
+	}
+}
