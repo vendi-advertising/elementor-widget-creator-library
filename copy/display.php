7564b803113737e0ddce3bc5fb6a84583f6c2aed
Chris Haas <chris@vendiadvertising.com>
2021-11-09 12:33:45 -0600

    Added escaping, separate PHP from HTML

--- copy/display.php
+++ copy/display.php
@@ -8,9 +8,15 @@ Developer Guide: https://github.com/WebsiteDons/elementor-widget-creator-library
 
 ?>
 <div class="myclass">
-<h2><?php echo $v->newwidgettext; ?></h2>
-<?php echo $v->newwidgettextarea; ?>
-<?php echo (!empty($v->newwidgetlink['url']) ? 
-'<p><a style="display:inline-block;background:#fff;padding:4px 10px;color:#222;border-radius:5px;line-height:normal;font-size:12px;" href="'.$v->newwidgetlink['url'].'"'.(!empty($v->newwidgetlink['is_external']) ? ' target="_blank"':'').'>See developer guide</a></p>'
-:''); ?>
+    <h2><?php esc_html_e($v->newwidgettext); ?></h2>
+    <?php echo wp_kses_post($v->newwidgettextarea); ?>
+    <?php if (!empty($v->newwidgetlink['url'])): ?>
+        <p><a
+                    style="display:inline-block;background:#fff;padding:4px 10px;color:#222;border-radius:5px;line-height:normal;font-size:12px;"
+                    href="<?php esc_attr_e($v->newwidgetlink['url']) ?>"
+                <?php if (!empty($v->newwidgetlink['is_external'])): ?>
+                    target="_blank"
+                <?php endif; ?>
+            >See developer guide</a></p>
+    <?php endif; ?>
 </div>
